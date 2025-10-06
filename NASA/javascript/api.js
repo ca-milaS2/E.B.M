49225cd0239fa7e0f3f40fe9c597b56e63918182
Kauã Cardoso <kauaanthony.cardoso44@gmail.com>
2025-10-05 21:39:17 -0300

    Delete NASA directory

--- NASA/javascript/api.js
+++
@@ -1,8 +0,0 @@
-
-fetch('/api/parsely-data')
-    .then(res => res.json())
-    .then(data => {
-        console.log('Dados da API:', data);
-        // Exibir no seu site
-    })
-    .catch(err => console.error('Erro:', err));
